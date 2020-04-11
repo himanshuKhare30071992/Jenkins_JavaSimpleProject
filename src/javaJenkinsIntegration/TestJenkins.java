5402c02900d93bdb17e7fc8f095a3102e8a42133
Himanshu <himanshukhare1992@gmail.com>
2020-04-11 16:43:07 +0530

    #1 Uploading complete project for first time

---
+++ src/javaJenkinsIntegration/TestJenkins.java
@@ -0,0 +1,19 @@
+package javaJenkinsIntegration;
+
+public class TestJenkins 
+{
+	
+
+	
+	public static void main(String[] args) 
+	{
+		System.out.println(System.getProperty("user.dir"));
+		
+		for(int i=1; i<=10; i++)
+		{
+			System.out.println(">>> Success: Jenkins is Integrated with Java....!!!");
+		}
+		
+	}
+
+}
